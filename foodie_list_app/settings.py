22e3ed6269d21482bd1aa334a68851e1f311fef0
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-06-28 16:09:13 +1000

    adding env and google api secret

--- foodie_list_app/settings.py
+++ foodie_list_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import dotenv
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
+
+load_dotenv(find_dotenv())
+
+GOOGLE_API_KEY= os.environ['GOOGLE_API_KEY']
+
 SECRET_KEY = 'django-insecure-*=)l89varyk78p4b8qj3da2jj$=8ni-ii#%i_lh4*e=#x$8jov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -116,7 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# STATICFILED_DIRS = [
+#     os.path.join(BASR_DIR, 'static')
+# ]
+
 STATIC_URL = 'static/'
+
+# STATIC_ROOT = os.path.join(BASR_DIR, 'static_cdn')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

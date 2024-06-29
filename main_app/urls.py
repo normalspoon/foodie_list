331d86ad42ee5ef8b91c4fd7f8441e2dcedd9f80
kiki luo <kikiluo0411@gmail.com>
2024-06-29 17:33:39 +1000

    Complete view template file.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('myLists/', views.myLists, name='myLists'),
+    path('myMap/', views.myMap, name='myMap'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def myLists(request):
+  return render(request, 'myLists.html')
+
+def myMap(request):
+  return render(request, 'myMap.html')

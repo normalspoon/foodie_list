8513afcac96e3bd1f5d5d6ab10d658369c5b3a92
Dylan Broad <dylan@Dylans-Air.home>
2024-06-29 17:27:40 +1000

    Adding User Auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+   
 ]

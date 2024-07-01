f2a17612710018163fd3983d8d4ddb1084b1c13f
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-07-01 11:48:07 +1000

    details page pull api data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from decouple import config
+import requests
 
 # Create your views here.
 def home(request):
@@ -19,3 +21,15 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+def places_details(request, place_id):
+  api_key = config('GOOGLE_API_KEY')
+  place_details_url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={api_key}'
+  response = requests.get(place_details_url)
+  place_details = response.json().get('result', {})
+
+  context = {
+    'place_details': place_details,
+    'api_key': api_key,
+  }
+  return render(request, 'restaurants/detail.html', context)

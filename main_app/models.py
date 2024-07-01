914811afe11708963675fc814699c5faed925c29
Dylan Broad <dylan@Dylans-Air.home>
2024-07-01 21:22:12 +1000

    Adding User Sign Up

--- main_app/models.py
+++ main_app/models.py
@@ -5,8 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50, default='User')
+  username = models.CharField(max_length=50)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   
+  def __str__(self):
+   return f'{self.name} ({self.id})'
   
   
   

bec4e88e016afb38086a5559334ddb5d9193de41
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-07-06 12:32:11 +1000

    comma add

--- foodie_list_app/settings.py
+++ foodie_list_app/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'foodie_list_app.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default= os.environ['DATABASE_URL']
+        default= os.environ['DATABASE_URL'],
         conn_max_age=600
     )
 }
